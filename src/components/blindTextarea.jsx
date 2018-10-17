d8d5fdd6203a5d3dc930084b71d8c50f39717ae1
claudio <cdjohnnatha@gmail.com>
2018-10-17 00:37:34 -0300

    create speech for mismatch

--- src/components/blindTextarea.jsx
+++ src/components/blindTextarea.jsx
@@ -3,9 +3,11 @@ import axios from 'axios';
 import Speech from 'speak-tts'
 import { Deserializer } from 'jsonapi-serializer';
 import SintaticAnalizer from '../helpers/sintaticAnalizer/sintaticAnalizer';
+const querystring = require('querystring');
 
 const dictionaryUrl = 'https://www.dicionarioweb.com.br/';
 const classificatorUrl = 'http://localhost:3001/api/v1/classification';
+const mismatchCheckerUrl = 'https://languagetool.org/api/v2/check';
 const speech = new Speech();
 const ptI18n = require('../i18n/pt-br');
 
@@ -21,6 +23,14 @@ class BlindTextarea extends Component {
       'splitSentences': false
     }
     speech.init(this.speachSetup);
+    this.mismatchRequestHeader = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    this.formMismatch = {
+      text: '',
+	    language: 'pt-BR',
+	    enabledOnly: false
+    }
   }
   prepareText(inputText) {
     return inputText.split(" ");
@@ -54,6 +64,27 @@ class BlindTextarea extends Component {
         const { data } = await axios({ method: 'post', url: classificatorUrl, data: this.buildJson(inputText) });
         const stackTokens = await new Deserializer().deserialize(data);
         new SintaticAnalizer(stackTokens);
+        this.formMismatch.text = inputText;
+        const { data: { matches: [mismatchErrors] } } = await axios({
+          method: 'post',
+          url: mismatchCheckerUrl,
+          headers: this.mismatchRequestHeader,
+          data: querystring.stringify(this.formMismatch) });
+          console.log(mismatchErrors);
+          console.log(mismatchErrors.message);
+          if (mismatchErrors.message === ptI18n.verbal_mismatch_error) {
+            let { replacements } = mismatchErrors;
+            replacements = replacements.map(element => element.value);
+            replacements = replacements.join(', ou, ');
+            await speech.speak({ text:
+              `
+                ${ptI18n.verbal_mismatch_error_speech}
+                ${inputText}
+                ${ptI18n.suggests}
+                ${replacements}
+               `
+              });
+          }
       } catch (error) {
         console.log(error);
         await speech.speak({ text: ptI18n.complexity_error_message});
